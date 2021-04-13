7760f838a8f29724f320c57e66812e7a6eeffb6e
thisissum <thisissunmh@gmail.com>
2021-04-13 18:18:13 +0800

    update get_data.py

--- mfm/data/utils.py
+++ mfm/data/utils.py
@@ -11,7 +11,7 @@ class DataConfig:
     instruments_name = "instruments.csv"
 
 
-def format_dt(datetime: Union[str, pd.Timestamp], api="tushare") -> str:
+def format_api_dt(datetime: Union[str, pd.Timestamp], api="tushare") -> str:
     if api == "tushare":
         if isinstance(datetime, str):
             datetime = pd.Timestamp(datetime)
@@ -19,3 +19,26 @@ def format_dt(datetime: Union[str, pd.Timestamp], api="tushare") -> str:
     else:
         raise ValueError("Api :{} not supported.".format(api))
     return datetime
+
+def format_local_dt(datetime: Union[str, int], api="tushare") -> pd.Timestamp:
+    if api == "tushare":
+        if isinstance(datetime, int):
+            datetime = str(datetime)
+        datetime = pd.to_datetime(datetime)
+    else:
+        raise ValueError("Api :{} not supported.".format(api))
+    return datetime
+
+def format_api_code(code: str, api: str = "tushare") -> str:
+    if api == "tushare":
+        code = ".".join([code[6:], code[:6]])
+    else:
+        raise ValueError("Api :{} not supported.".format(api))
+    return code
+
+def format_local_code(code: str, api: str = "tushare") -> str:
+    if api == "tushare":
+        code = "".join(code.split('.')[::-1])
+    else:
+        raise ValueError("Api :{} not supported.".format(api))
+    return code
